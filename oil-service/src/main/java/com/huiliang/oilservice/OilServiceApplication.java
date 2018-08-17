cd1a4cd3b89e7861087e227edbdc40ecf87608fe
lijinghao1997 <lijinghao1997@outlook.com>
2018-08-17 15:47:47 +0800

    第一次提交

---
+++ oil-service/src/main/java/com/huiliang/oilservice/OilServiceApplication.java
@@ -0,0 +1,12 @@
+package com.huiliang.oilservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilServiceApplication.class, args);
+	}
+}
---
+++ sales-situation-service/src/main/java/com/huiliang/salessituationservice/SalesSituationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.huiliang.salessituationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSituationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSituationServiceApplication.class, args);
+	}
+}
